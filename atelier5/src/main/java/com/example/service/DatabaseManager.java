246ec3afee86a811dfe4e7bf9cd8cc81b2715150
paisible7 <22km127@esisalama.org>
2025-04-18 22:10:46 +0200

    COMMIT_18_AVRIL_2025 ==> methode creer

--- atelier5/src/main/java/com/example/service/DatabaseManager.java
+++ atelier5/src/main/java/com/example/service/DatabaseManager.java
@@ -1,6 +1,9 @@
 package com.example.service;
 
 import com.example.app.Produit;
+import com.example.app.ProduitAlimentaire;
+import com.example.app.ProduitCosmetique;
+import com.example.app.ProduitMenager;
 import com.example.user.*;
 import java.sql.*;
 import java.util.ArrayList;
@@ -15,16 +18,60 @@ public class DatabaseManager {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
-    public static List<Produit> chargerProduits(){
+    public static List<Produit> chargerProduits() {
         List<Produit> produits = new ArrayList<>();
         String sql = "SELECT * FROM produits";
 
-        try {
-            
-        } catch (Exception e) {
+        try (
+                Connection conn = getConnection();
+                Statement stmt = conn.createStatement();
+                ResultSet rslt = stmt.executeQuery(sql)) {
+            while (rslt.next()) {
+                String type = rslt.getString("type");
+                Produit p = creerProduit(rslt, type);
+                produits.add(p);
+            }
+
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
         return produits;
+    }
+
+    private static Produit creerProduit(ResultSet rslt, String type) throws SQLException {
+
+        switch (type.toLowerCase()) {
+            case "alimentaire":
+                return new ProduitAlimentaire(
+                        rslt.getInt("id"),
+                        rslt.getString("nom"),
+                        rslt.getDouble("prix"),
+                        rslt.getInt("quantite"),
+                        rslt.getString("date_expiration"),
+                        rslt.getString("origine"));
+            case "menager":
+                return new ProduitMenager(
+                        rslt.getInt("id"),
+                        rslt.getString("nom"),
+                        rslt.getDouble("prix"),
+                        rslt.getInt("quantite"),
+                        rslt.getString("type_menager"),
+                        rslt.getString("date_fabrication"));
+            case "cosmetique":
+                return new ProduitCosmetique(
+                        rslt.getInt("id"),
+                        rslt.getString("nom"),
+                        rslt.getDouble("prix"),
+                        rslt.getInt("quantite"),
+                        rslt.getString("type_peau"),
+                        rslt.getBoolean("teste_sur_animaux"));
+            default:
+                return new Produit(
+                        rslt.getInt("id"),
+                        rslt.getString("nom"),
+                        rslt.getDouble("prix"),
+                        rslt.getInt("quantite"));
+        }
     }
 }
