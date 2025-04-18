1cc20f042a2eb82af5440fc580708340762e956b
paisible7 <22km127@esisalama.org>
2025-04-18 21:30:15 +0200

    COMMIT_18_AVRIL_2025 ==> declaration des attributs et de la liste des produits

--- atelier5/src/main/java/com/example/service/DatabaseManager.java
+++ atelier5/src/main/java/com/example/service/DatabaseManager.java
@@ -1,10 +1,30 @@
 package com.example.service;
 
+import com.example.app.Produit;
 import com.example.user.*;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseManager {
-    
+    private static final String URL = "jdbc:mysql://localhost:3306/supermarche";
+    private static final String USER = "root";
+    private static final String PASSWORD = "admin123mdp";
+
+    private static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static List<Produit> chargerProduits(){
+        List<Produit> produits = new ArrayList<>();
+        String sql = "SELECT * FROM produits";
+
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return produits;
+    }
 }
